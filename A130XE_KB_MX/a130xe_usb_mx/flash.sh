65abdd424c461745dc7b28e1013cbd45a4a4e021
RetroBitLab <icjtqr@gmail.com>
2021-01-09 23:25:15 +0100

    First import from tmk_keyboard

--- A130XE_KB_MX/a130xe_usb_mx/flash.sh
+++ A130XE_KB_MX/a130xe_usb_mx/flash.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 openocd -f openocd/stm32f4eval.cfg \
--c "init; targets; reset init; wait_halt; poll; flash write_image erase unlock build/a130xe_usb_mx.elf; reset run; shutdown"
+-c "init; targets; reset init; wait_halt; poll; flash write_image erase unlock build/matrix_key_usb.elf; reset run; shutdown"
