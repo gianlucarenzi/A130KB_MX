50ed4237266af9d5da92912f0042c35ff2e3fa36
RetroBitLab <icjtqr@gmail.com>
2021-01-10 19:09:25 +0100

    Change variable name and type. Now it is an array for Alternate Functions

--- A130XE_KB_MX/a130xe_usb_mx/Core/Src/amiga_config.c
+++ A130XE_KB_MX/a130xe_usb_mx/Core/Src/amiga_config.c
@@ -8,20 +8,22 @@
 #define KC_SPARE_2 KC_NO
 #define KC_SPARE_3 KC_NO
 
-uint8_t keyboard_matrix_amiga[KEYBOARD_ROWS][KEYBOARD_COLUMNS] = {
-	{ KC_ESC,     KC_KP_LPAREN, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_KP_RPAREN, KC_F6,   KC_PSLS, KC_F7,   KC_F8,      KC_F9,      KC_F10,  KC_HELP,  KC_NO },
-	{ KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,    KC_EQL,     KC_BSLS, KC_UP,    KC_NO },
-	{ KC_TAB,     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,    KC_RBRC,    KC_ENT,  KC_LEFT,  KC_NO },
-	{ KC_CAPS,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,    KC_SPARE_1, KC_DEL,  KC_RIGHT, KC_NO },
-	{ KC_SPARE_2, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SPARE_3, KC_SPC,     KC_BSPC, KC_DOWN,  KC_NO },
-	{ KC_PAST,    KC_PPLS, KC_P9,   KC_P6,   KC_P3,   KC_PCMM, KC_P8,   KC_P5,   KC_P2,   KC_PENT, KC_P7,   KC_P4,      KC_P1,      KC_P0,   KC_PMNS,  KC_NO },
-	{ KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,    KC_NO,    KC_NO, KC_NO,  KC_RSFT },
-	{ KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,    KC_NO,    KC_NO, KC_NO,  KC_RALT },
-	{ KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,    KC_NO,    KC_NO, KC_NO,  KC_RGUI },
-	{ KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,    KC_NO,    KC_NO, KC_NO,  KC_LCTRL },
-	{ KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,    KC_NO,    KC_NO, KC_NO,  KC_LSFT },
-	{ KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,    KC_NO,    KC_NO, KC_NO,  KC_LALT },
-	{ KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,    KC_NO,    KC_NO, KC_NO,  KC_LGUI },
+const uint8_t keymaps[][KEYBOARD_ROWS][KEYBOARD_COLUMNS] = {
+	[0] = {
+		{ KC_ESC,     KC_KP_LPAREN, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_KP_RPAREN, KC_F6,   KC_PSLS, KC_F7,   KC_F8,      KC_F9,      KC_F10,  KC_HELP,  KC_NO },
+		{ KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,    KC_EQL,     KC_BSLS, KC_UP,    KC_NO },
+		{ KC_TAB,     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,    KC_RBRC,    KC_ENT,  KC_LEFT,  KC_NO },
+		{ KC_CAPS,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,    KC_SPARE_1, KC_DEL,  KC_RIGHT, KC_NO },
+		{ KC_SPARE_2, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SPARE_3, KC_SPC,     KC_BSPC, KC_DOWN,  KC_NO },
+		{ KC_PAST,    KC_PPLS, KC_P9,   KC_P6,   KC_P3,   KC_PCMM, KC_P8,   KC_P5,   KC_P2,   KC_PENT, KC_P7,   KC_P4,      KC_P1,      KC_P0,   KC_PMNS,  KC_NO },
+		{ KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,    KC_NO,    KC_NO, KC_NO,  KC_RSFT },
+		{ KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,    KC_NO,    KC_NO, KC_NO,  KC_RALT },
+		{ KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,    KC_NO,    KC_NO, KC_NO,  KC_RGUI },
+		{ KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,    KC_NO,    KC_NO, KC_NO,  KC_LCTRL },
+		{ KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,    KC_NO,    KC_NO, KC_NO,  KC_LSFT },
+		{ KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,    KC_NO,    KC_NO, KC_NO,  KC_LALT },
+		{ KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,    KC_NO,    KC_NO, KC_NO,  KC_LGUI },
+	},
 };
 
 gpioPort_t lut_row_amiga[ KEYBOARD_ROWS ] = {
